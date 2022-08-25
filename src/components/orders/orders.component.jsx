3a5a4c95f93cef0d416e4a3a56e23a0fb2ecf050
NitinHegde278 <nhnitinhegde24@gmail.com>
2022-08-25 18:35:43 +0530

    removed unused state

--- src/components/orders/orders.component.jsx
+++ src/components/orders/orders.component.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import NoOrderCard from "../no-order-card/no-order-card.component";
 import Card from "../utils/card/card.component";
 import OrderCard from "../utils/order-card/order-card.component";
 import "./orders.styles.css";
 
 const Orders = () => {
-  const [orders, setOrders] = useState([]);
+  const orders = [];
   return (
     <>
       {orders.length ? (
